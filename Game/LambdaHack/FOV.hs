7422759e59f97d0c2e0a0126c35493e50ab2aa38
Mikolaj <mikolaj.konarski@gmail.com>
2012-01-15 17:17:49 +0100

    Add FOV radius via Angband metric

--- Game/LambdaHack/FOV.hs
+++ Game/LambdaHack/FOV.hs
@@ -24,20 +24,30 @@ data FovMode = Shadow | Permissive | Digital | Blind
 -- Press a command key in the game to cycle among the algorithms
 -- and see a special visualization of their effects..
 fullscan :: FovMode -> Int -> Loc -> Kind.Ops TileKind -> Level -> [Loc]
-fullscan fovMode _radius loc cops Level{lxsize, lmap} =
+fullscan fovMode r loc cops Level{lxsize, lmap} =
   case fovMode of
     Shadow ->
-      L.concatMap (\ tr -> map tr (Shadow.scan (isCl . tr) 1 (0, 1))) tr8
+      L.concatMap (\ tr -> map tr (Shadow.scan (isClear . tr) 1 (0, 1))) tr8
     Permissive ->
-      L.concatMap (\ tr -> map tr (Permissive.scan (isCl . tr))) tr4
+      L.concatMap (\ tr -> map tr (Permissive.scan (isCl r tr))) tr4
     Digital ->
-      L.concatMap (\ tr -> map tr (Digital.scan (isCl . tr))) tr4
+      L.concatMap (\ tr -> map tr (Digital.scan (isCl r tr))) tr4
     Blind ->  -- only feeling out adjacent tiles by touch
-      let _radiusOne = 1 :: Int
-      in L.concatMap (\ tr -> map tr (Digital.scan (isCl . tr))) tr4
+      let radiusOne = 1
+      in L.concatMap (\ tr -> map tr (Digital.scan (isCl radiusOne tr))) tr4
  where
-  isCl :: Loc -> Bool
-  isCl = Tile.isClear cops . (lmap Kind.!)
+  isClear :: Loc -> Bool
+  isClear = Tile.isClear cops . (lmap Kind.!)
+
+  -- Uses Angband metric. Unfortunately this is not compatible
+  -- with the movement metric, so melee characters would prefer
+  -- to approach their foes diagonally and ranged characters would prefer
+  -- vertical and horizontal lines. That's unnatural and too complex.
+  isCl :: Int -> (Bump -> Loc) -> Bump -> Bool
+  isCl radius tr b@(B(x', y')) =
+    let (x, y) = (abs x', abs y')
+    in max x y + min x y `div` 2 < radius
+       && isClear (tr b)
 
   trL = trLoc lxsize loc
 
