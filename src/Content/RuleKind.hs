18cd6b436a4b17e03d9af70efc6c279f49ee334b
Mikolaj <mikolaj.konarski@gmail.com>
2012-10-27 00:54:30 +0200

    Add the ASCII art content for the Main Menu

--- src/Content/RuleKind.hs
+++ src/Content/RuleKind.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 -- Copyright (c) 2008--2011 Andres Loeh, 2010--2012 Mikolaj Konarski
 -- This file is a part of the computer game Allure of the Stars
 -- and is released under the terms of the GNU Affero General Public License.
@@ -5,6 +6,8 @@
 --
 -- | Game rules and assorted data for Allure of the Stars.
 module Content.RuleKind ( cdefs ) where
+
+import Multiline
 
 -- Cabal
 import qualified Paths_Allure as Self (getDataFileName, version)
@@ -39,4 +42,46 @@ standard = RuleKind
   , rpathsVersion  = Self.version
   , ritemMelee     = "/|\\"
   , ritemProject   = "!?,-~}{"
-  }
+  -- ASCII art for the Main Menu. Only pure 7-bit ASCII characters are
+  -- allowed. The picture should be exactly 24 rows by 80 columns,
+  -- plus an extra frame of any charecters that is ignored for all purposes.
+  -- For a different screen size, the picture is centered and the outermost
+  -- rows and columns cloned. When displayed in the Main Menu screen,
+  -- it's overwritten with the game version string and keybinding strings.
+  -- The game version string begins and ends with a space and is placed
+  -- in the very bottom right corner. The keybindings overwrite places
+  -- marked with 25 starting brace signs '{' in a row. The sign is forbidden
+  -- everywhere else. Exactly five such places with 25 starting braces
+  -- are required, at most one per row, and all are overwritten
+  -- with text that is flushed left and padded with spaces.
+  -- The Main Menu is displayed dull white on black.
+  -- TODO: Highlighted keybinding is in inverse video or bright white on grey
+  -- background. The spaces that pad keybindings are not highlighted.
+  , rmainMenuArt   = [multiline|
+----------------------------------------------------------------------------------
+|                                                                                |
+|                                                                                |
+|                      >> Allure of the Stars <<                                 |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                      {{{{{{{{{{{{{{{{{{{{{{{{{                                 |
+|                                                                                |
+|                      {{{{{{{{{{{{{{{{{{{{{{{{{                                 |
+|                                                                                |
+|                      {{{{{{{{{{{{{{{{{{{{{{{{{                                 |
+|                                                                                |
+|                      {{{{{{{{{{{{{{{{{{{{{{{{{                                 |
+|                                                                                |
+|                      {{{{{{{{{{{{{{{{{{{{{{{{{                                 |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                                                                                |
+|                        Version X.X.X (frontend: gtk, engine: LambdaHack X.X.X) |
+----------------------------------------------------------------------------------
+|]}
