7d1231d32216abb3deb52791f08ac3407c6fa100
Mikolaj <mikolaj.konarski@gmail.com>
2013-12-16 17:50:27 +0100

    Don't use any dark paths
    We don't have colours for them, anyway (ideally we need 2 colours
    per feature and separate 2 for its dark variant, and for paths
    we only have 2 total, 3 if we use yellow for currently seen dark tile,
    just as in case of normal floor).

--- src/Content/CaveKind.hs
+++ src/Content/CaveKind.hs
@@ -64,7 +64,7 @@ arena = rogue
   , chidden       = 1000
   , citemNum      = rollDice 5 2  -- few rooms
   , cdefTile      = "arenaSet"
-  , cdarkCorTile  = "pathDark"
+  , cdarkCorTile  = "pathLit"  -- let paths around rooms be lit
   , clitCorTile   = "pathLit"
   , couterFenceTile = "oriels fence"
   }
@@ -84,7 +84,7 @@ empty = rogue
   , chidden       = 1000
   , citemNum      = rollDice 8 2  -- whole floor strewn with treasure
   , cdefTile      = "emptySet"
-  , cdarkCorTile  = "pathDark"
+  , cdarkCorTile  = "pathLit"  -- let paths around rooms be lit
   , clitCorTile   = "floorArenaLit"
   , couterFenceTile = "oriels fence"
   }
@@ -100,7 +100,7 @@ noise = rogue
   , chidden       = 1000
   , citemNum      = rollDice 4 2  -- fewer rooms
   , cdefTile      = "noiseSet"
-  , cdarkCorTile  = "pathDark"
+  , cdarkCorTile  = "pathLit"  -- let paths give off light
   , clitCorTile   = "pathLit"
   }
 combat = rogue
@@ -119,6 +119,6 @@ combat = rogue
   , citemNum      = rollDice 12 2
   , citemFreq     = [(100, "useful")]
   , cdefTile      = "combatSet"
-  , cdarkCorTile  = "pathLit"  -- for now, let paths give off light
+  , cdarkCorTile  = "pathLit"  -- let paths give off light
   , clitCorTile   = "floorArenaLit"
   }
