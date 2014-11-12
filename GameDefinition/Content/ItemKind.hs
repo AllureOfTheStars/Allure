e86a70ee1a19fb7f3cc4713da0674c7fef781c63
Mikolaj <mikolaj.konarski@gmail.com>
2014-11-12 20:27:09 +0100

    Strengthen items that add moves, since you pay one move to use them

--- GameDefinition/Content/ItemKind.hs
+++ GameDefinition/Content/ItemKind.hs
@@ -171,7 +171,7 @@ jumpingPole = ItemKind
   , irarity  = [(1, 2), (10, 1)]
   , iverbHit = "prod"
   , iweight  = 10000
-  , iaspects = [Timeout $ (d 3 + 3 - dl 3) |*| 10]
+  , iaspects = [Timeout $ (d 2 + 2 - dl 2) |*| 10]
   , ieffects = [Recharging (toOrganActorTurn "fast 20" 1)]
   , ifeature = [Durable, Applicable, Identified]
   , idesc    = "Makes you vulnerable at take-off, but then you are free like a bird."
@@ -426,7 +426,7 @@ potion5 = potion
 potion6 = potion
   { irarity  = [(3, 3), (10, 6)]
   , ieffects = [ OneOf [ Dominate, DropBestWeapon, RefillHP 20, Burn 9
-                       , InsertMove 2 ]
+                       , InsertMove 4 ]
                , OnSmash (OneOf [ Explode "healing mist 2"
                                 , Explode "healing mist 2"
                                 , Explode "pheromone"
@@ -446,8 +446,7 @@ potion8 = potion
                , OnSmash (Explode "antidote mist") ]
   }
 potion9 = potion
-  { ieffects = [ NoEffect "of nullification"
-               , DropItem COrgan "temporary conditions" True
+  { ieffects = [ DropItem COrgan "temporary conditions" True
                , OnSmash (Explode "blast 10") ]
   }
 
@@ -592,14 +591,14 @@ scroll3 = scroll
   }
 scroll4 = scroll
   { ieffects = [ OneOf [ Teleport $ d 3 |*| 3, RefillCalm 10, RefillCalm (-10)
-                       , InsertMove 4, Paralyze 10, Identify CGround ] ]
+                       , InsertMove 3, Paralyze 10, Identify CGround ] ]
   }
 scroll5 = scroll
   { irarity  = [(3, 3), (10, 6)]
   , ieffects = [ OneOf [ Summon standardSummon $ d 2
                        , CallFriend 1, Ascend (-1), Ascend 1
-                      , RefillCalm 30, RefillCalm (-30)
-                      , CreateItem CGround "useful" TimerNone
+                       , RefillCalm 30, RefillCalm (-30)
+                       , CreateItem CGround "useful" TimerNone
                        , PolyItem CGround ] ]
                -- TODO: ask player: Escape 1
   }
@@ -608,7 +607,7 @@ scroll6 = scroll
   }
 scroll7 = scroll
   { irarity  = [(10, 3)]
-  , ieffects = [InsertMove $ d 2 + dl 2]
+  , ieffects = [InsertMove $ 1 + d 2 + dl 2]
   }
 scroll8 = scroll
   { irarity  = [(3, 8), (10, 4)]
