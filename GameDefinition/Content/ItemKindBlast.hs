2caa03931a2679d6b2f18bec7f7bf4e45e149477
Mikolaj <mikolaj.konarski@gmail.com>
2015-03-09 12:27:45 +0100

    Set linger to 10 for explosions that should last 1 turns only

--- GameDefinition/Content/ItemKindBlast.hs
+++ GameDefinition/Content/ItemKindBlast.hs
@@ -104,7 +104,9 @@ fragrance = ItemKind
   , iweight  = 1
   , iaspects = []
   , ieffects = [Impress]
-  , ifeature = [ ToThrow $ ThrowMod 28 20  -- 2 steps, one turn
+  -- Linger 10, because sometimes it takes 2 turns due to starting just
+  -- before actor turn's end (e.g., via a necklace).
+  , ifeature = [ ToThrow $ ThrowMod 28 10  -- 2 steps, one turn
                , Fragile, Identified ]
   , idesc    = ""
   , ikit     = []
@@ -232,7 +234,7 @@ waste = ItemKind
   , iweight  = 50
   , iaspects = []
   , ieffects = [RefillHP (-1)]
-  , ifeature = [ ToThrow $ ThrowMod 28 20  -- 2 steps, one turn
+  , ifeature = [ ToThrow $ ThrowMod 28 10  -- 2 steps, one turn
                , Fragile, Identified ]
   , idesc    = ""
   , ikit     = []
