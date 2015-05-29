f32851e7b3d0eea9c3b38c7d9f79a0703599cadb
Mikolaj <mikolaj.konarski@gmail.com>
2015-05-29 13:15:06 +0200

    Update player definitions vs engine changes

--- GameDefinition/Content/ModeKindPlayer.hs
+++ GameDefinition/Content/ModeKindPlayer.hs
@@ -191,13 +191,3 @@ hiRaid = [ ( [(HiLoot, 1)]
          , ( [(HiConst, 100)]
            , victoryOutcomes )
          ]
-
-minusTen, meleeAdjacent, _meleeAndRanged :: Skills
-
--- To make sure only a lot of weak items can override move-only-leader, etc.
-minusTen = EM.fromList $ zip [minBound..maxBound] [-10, -10..]
-
-meleeAdjacent = EM.delete AbWait $ EM.delete AbMelee minusTen
-
--- Melee and reaction fire.
-_meleeAndRanged = EM.delete AbProject meleeAdjacent
