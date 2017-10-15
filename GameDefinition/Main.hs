e5ff27c88abc55359c491f27b8c655ff9fd11a30
Mikolaj <mikolaj.konarski@gmail.com>
2017-10-15 17:11:07 +0200

    Update vs commandline changes in LH

--- GameDefinition/Main.hs
+++ GameDefinition/Main.hs
@@ -15,18 +15,19 @@ import Game.LambdaHack.Common.Prelude
 
 import Control.Concurrent.Async
 import qualified Control.Exception as Ex
-import System.Environment (getArgs)
+import qualified Options.Applicative as OA
 import System.Exit
 
+import Game.LambdaHack.Server (debugModeSerPI)
 import TieKnot
 
 -- | Tie the LambdaHack engine client, server and frontend code
 -- with the game-specific content definitions, and run the game.
 main :: IO ()
 main = do
-  args <- getArgs
+  debugModeSer <- OA.execParser debugModeSerPI
   -- Avoid the bound thread that would slow down the communication.
-  a <- async $ tieKnot args
+  a <- async $ tieKnot debugModeSer
   ex <- waitCatch a
   case ex of
     Right () -> return ()
