f5358685e28778fa67e9f6eab311ac009560fb4f
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-02-12 11:18:22 +0100

    Add some greenery to parks

--- GameDefinition/Content/TileKind.hs
+++ GameDefinition/Content/TileKind.hs
@@ -199,7 +199,7 @@ tree = TileKind
   { tsymbol  = 'O'
   , tname    = "tree"
   , tfreq    = [ ("brawlSetLit", 140), ("shootoutSetLit", 10)
-               , ("escapeSetLit", 30), ("treeShadeOver_O_Lit", 1) ]
+               , ("escapeSetLit", 35), ("treeShadeOver_O_Lit", 1) ]
   , tcolor   = BrGreen
   , tcolor2  = Green
   , talter   = 50
@@ -394,7 +394,7 @@ pulpit = TileKind
 bush = TileKind
   { tsymbol  = '%'
   , tname    = "bush"
-  , tfreq    = [ ("bush Lit", 1), ("shootoutSetLit", 30), ("escapeSetLit", 30)
+  , tfreq    = [ ("bush Lit", 1), ("shootoutSetLit", 30), ("escapeSetLit", 40)
                , ("arenaSetLit", 3), ("bushClumpOver_f_Lit", 1) ]
   , tcolor   = BrGreen
   , tcolor2  = Green
@@ -437,7 +437,7 @@ floorFog = TileKind
   }
 floorFogDark = floorFog
   { tname    = "thick fog"
-  , tfreq    = [("noiseSet", 10), ("escapeSetDark", 60)]
+  , tfreq    = [("noiseSet", 10), ("escapeSetDark", 50)]
   , tfeature = Dark : tfeature floorFog
   }
 floorSmoke = TileKind
