d88e386bcd4315c524e304333661f7e38ad18888
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-03-08 08:17:19 +0100

    Prevent premature identification of 'Letting Go'

--- GameDefinition/Content/ItemKindOrgan.hs
+++ GameDefinition/Content/ItemKindOrgan.hs
@@ -1081,7 +1081,8 @@ backstoryNeutralTemplate = backstoryFluffTemplate
   }
 backstoryNeutral1 = backstoryNeutralTemplate
   { iname    = "\"Letting Go\""
-  , ifreq    = [(BACKSTORY_NEUTRAL, 100), (BACKSTORY, 1)]
+  , ifreq    = [ (BACKSTORY_NEUTRAL, 100), (BACKSTORY, 1)
+               , (VALUABLE, 1000) ]  -- prevent premature identification
   , ieffects = [OnSmash (Explode S_YOUTH_SPRINKLE)]  -- may hit foes as well
   , idesc    = "Quirk: Dying beautifully is an art that takes a lifetime to master and leaves spectators peaceful and uplifted. That's true even for clinical death, potentially reversible with nano medbot treatment back in town."
   }
